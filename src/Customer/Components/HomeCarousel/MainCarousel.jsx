6fa9896115048fbf14d019f89fba1a4aa0f493b3
Joshi Harsh <harshjoshi3008@gmail.com>
2024-08-30 15:50:46 +0530

    Carousel Setup

---
+++ src/Customer/Components/HomeCarousel/MainCarousel.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { MainCarouselData } from './MainCarouselData';
+import AliceCarousel from 'react-alice-carousel';
+import "react-alice-carousel/lib/alice-carousel.css";
+
+const MainCarousel = () => {
+
+  const items = MainCarouselData.map((item) => (
+    <img
+      className="cursor-pointer"
+      role="presentation"
+      src={item.image}
+      alt=""
+    />
+  ));
+  
+  return (
+    <AliceCarousel
+      items={items}
+      disableButtonsControls
+      autoPlay
+      autoPlayStrategy=''
+      autoPlayInterval={3000}
+      infinite
+    />
+  );
+}
+
+export default MainCarousel
---
+++ src/Customer/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import MainCarousel from '../../Components/HomeCarousel/MainCarousel'
+
+const HomePage = () => {
+  return (
+      <div>
+          <MainCarousel />
+          
+          <div>
+              Other Sections
+          </div>
+    </div>
+  )
+}
+
+export default HomePage
